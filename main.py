ecbddacd0fa4825956f3903653fc85aef1b60b4e
Aksinya Sokolova <asokolova@team.amocrm.com>
2022-10-05 23:29:14 +0300

    refactor some files from the first meeting

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from course_python import ls1
+from course_python import ls1_functions
 from course_python import ls2_functions
 from course_python import ls2_jsons
 
 if __name__ == '__main__':
-    ls1.print_hi('PyCharm')
-    result = ls1.count_square()
+    ls1_functions.print_hi('PyCharm')
+    result = ls1_functions.count_square()
 
     ls2_functions.test_function2(5, 6, 7, 8, 9, 20)
     ls2_functions.test_function3(value1=40, value2=50, value3=100)
