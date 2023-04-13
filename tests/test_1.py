06f92a89adc6638d2a009ff5e29ab26771ae8d9a
Aks S <aks.and.space@gmail.com>
2023-04-13 19:33:36 +0400

    first test

---
+++ tests/test_1.py
@@ -0,0 +1,36 @@
+# написать цикл, который распечатает каждый элемент списка
+cars = ["toyota", "nissan", "rolls royce", "peugeot", "porsche", "lexus", "opel",
+      "volkswagen"]
+print("I bought %машина%")
+
+
+# найти количество букв 'l' в строке
+# Разница между capitalize, title, upper, lower?
+# найти первое упоминание буквы d
+test_string = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
+
+# Объединить 2 списка в один, чтобы образовались пары: "страна - столица".
+# Распечатать сообщение: "%capital% is the capital of %country%"
+
+countries = ['Japan', 'Canada', 'Romania', 'USA', 'Germany', 'India', 'Australia']
+capitals = ['Tokyo', 'Toronto', 'Buharest', 'Washington', 'Berlin', 'New Delhi', 'Canberra']
+
+# Принимать возраст с клавиатуры, затем распечатывать поздравление с днём рождения с упоминанием возраста.
+# Пример сообщения: "Happy 23rd Birthday!"
+
+age = 23
+message = "Happy " + str(age) + "rd Birthday!"
+print(message)
+
+# 1 = first = 1st
+# 2 = second = 2nd
+# 3 = third = 3rd
+# 4 = fourth = 4th
+# 5 = fifth = 5th
+# 6 = sixth = 6th
+# 7 = seventh = 7th
+# ...
+# 21 = 21st
+# 22 = 22nd
+# 23 = 23rd
+# ...
